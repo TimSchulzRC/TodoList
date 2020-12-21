d4783487de451ad1a0d6b3012bc87a9f9a7989f2
TimSchulzRC <TimSchulzRC@github.com>
2020-12-21 20:58:05 +0100

    finish second try successfully

--- app.js
+++ app.js
@@ -4,31 +4,29 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-var items = [];
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+};
 
-app.set("view engine", "ejs");
+items = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  var day = today.toLocaleDateString("de-DE", options);
-
-  res.render("list", { day: day, listItems: items });
+  date = new Date();
+  date = date.toLocaleString("de-DE", options);
+  res.render("list", { date: date, listItems: items });
 });
 
-app.post("/", function (req, res) {
-  var item = req.body.newItem;
-  items.push(item);
+app.post("/", (req, res) => {
+  items.push(req.body.newItem);
   res.redirect("/");
 });
 
-app.listen(port, function () {
-  console.log("server started");
+app.listen(3000, function () {
+  console.log("server running on port " + port);
 });
