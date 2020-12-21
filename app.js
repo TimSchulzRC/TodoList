06e137bbe7b07f55f361aa51ea0dfd925a058bd3
TimSchulzRC <TimSchulzRC@github.com>
2020-12-21 21:57:14 +0100

    make second list and apply layout

--- app.js
+++ app.js
@@ -4,27 +4,42 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 const options = {
   weekday: "long",
   day: "numeric",
   month: "long",
 };
 
-items = [];
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
+let items = [];
+let workItems = [];
 
 app.get("/", function (req, res) {
-  date = new Date();
+  let date = new Date();
   date = date.toLocaleString("de-DE", options);
-  res.render("list", { date: date, listItems: items });
+  res.render("list", { listTitle: date, listItems: items });
 });
 
 app.post("/", (req, res) => {
-  items.push(req.body.newItem);
-  res.redirect("/");
+  let item = req.body.newItem;
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", listItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, function () {
